12f6afe4c1535a91fe5cbef5ca7dfc76df88d02a
Rahul Tiwari <rt699652@gmail.com>
2025-09-27 16:54:10 +0530

    updated

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -5,17 +5,19 @@ import "./Tooltip.css";
 function Tooltip({ text, children }) {
   const [visible, setVisible] = useState(false);
 
-  return (
-    <span
-      className="tooltip"
-      onMouseEnter={() => setVisible(true)}
-      onMouseLeave={() => setVisible(false)}
-    >
-      {children}
-      {visible && <div className="tooltiptext">{text}</div>}
-    </span>
-  );
+  return React.cloneElement(children, {
+    className: (children.props.className || "") + " tooltip",
+    onMouseEnter: () => setVisible(true),
+    onMouseLeave: () => setVisible(false),
+    children: (
+      <>
+        {children.props.children}
+        {visible && <div className="tooltiptext">{text}</div>}
+      </>
+    ),
+  });
 }
 
 export default Tooltip;
+
 
