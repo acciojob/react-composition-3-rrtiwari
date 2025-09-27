aeb487d44052a8705170bd1dcdd45bebcb805d4c
Rahul Tiwari <rt699652@gmail.com>
2025-09-27 18:47:03 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,20 @@
 import React from "react";
 import Tooltip from "./Tooltip";
 import "./Tooltip.css";
+
 function App() {
   return (
     <div className="App" style={{ padding: "50px", textAlign: "center" }}>
       <Tooltip text="This is a tooltip">
         <h2>Hover over me</h2>
       </Tooltip>
+
       <Tooltip text="This is another tooltip">
         <p>Hover over me to see another tooltip</p>
       </Tooltip>
     </div>
   );
 }
+
 export default App;
+
