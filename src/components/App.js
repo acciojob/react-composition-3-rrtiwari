47d4c7780a387939e96058c40d39523058d5a12a
Rahul Tiwari <rt699652@gmail.com>
2025-09-27 18:22:22 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,24 @@
 
 import React from "react";
+import Tooltip from "./Tooltip";
 import "./Tooltip.css";
 
 function App() {
   return (
     <div className="App" style={{ padding: "50px", textAlign: "center" }}>
+      <Tooltip text="This is a tooltip">
+        <h2>Hover over me</h2>
+      </Tooltip>
 
-      <h2 className="tooltip">
-        Hover over me
-        <div className="tooltiptext">This is a tooltip</div>
-      </h2>
-
-      <p className="tooltip">
-        Hover over me to see another tooltip
-        <div className="tooltiptext">This is another tooltip</div>
-      </p>
-
+      <Tooltip text="This is another tooltip">
+        <p>Hover over me to see another tooltip</p>
+      </Tooltip>
     </div>
   );
 }
 
 export default App;
+
 
 
 
--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,17 +1,22 @@
 
-import React from "react";
+import React, { useState } from "react";
+import "./Tooltip.css";
 
 export default function Tooltip({ text, children }) {
-  return React.cloneElement(children, {
-    className: (children.props.className || "") + " tooltip",
-    children: (
-      <>
-        {children.props.children}
-        <div className="tooltiptext">{text}</div>
-      </>
-    ),
-  });
+  const [hover, setHover] = useState(false);
+
+  return (
+    <div
+      className="tooltip"
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      {children}
+      <div className={`tooltiptext ${hover ? "show" : ""}`}>{text}</div>
+    </div>
+  );
 }
+
 
 
 
